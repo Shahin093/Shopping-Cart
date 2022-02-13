80e73126bdaa88c0f201cf5cdcffe1fb2d3cb281
Shidul Islam <sishahin093@gmail.com>
2022-02-13 11:01:43 +0600

    shopping cart handle decrease and update price

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-document.getElementById('case-plus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-plus-count');
-    const caseCount = caseInput.value;
-    caseInput.value = parseInt(caseCount) + 1;
+function updateCaseNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-count');
+    let caseCount = caseInput.value;
+    if (isIncreasing == true) {
+        caseCount = parseInt(caseCount) + 1;
+    } else {
+        if (caseCount > 0) {
+            caseCount = parseInt(caseCount) - 1;
+        }
+    }
+    caseInput.value = caseCount;
+    // Update case total 
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseCount * 59;
+}
 
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true);
 });
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
+});
+
