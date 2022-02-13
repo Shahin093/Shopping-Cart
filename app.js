3a79ee7de43ce7d0d28d81be418cd5b9ac5ce120
Shidul Islam <sishahin093@gmail.com>
2022-02-13 12:37:45 +0600

    28-4 Get all product sub total cost, tax and grand total

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ function updateProductNumber(product, price, isIncreasing) {
     // Update total 
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    // calculate total 
+    calculateTotal();
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal() {
+
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on the html
+    document.getElementById('Subtotal').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+
+
 }
 
 // phone increase decrease events
@@ -20,7 +42,7 @@ document.getElementById('phone-plus').addEventListener('click', function () {
 });
 document.getElementById('phone-minus').addEventListener('click', function () {
     updateProductNumber('phone', 1219, false);
-})
+});
 // handle case increase decrease events 
 document.getElementById('case-plus').addEventListener('click', function () {
     updateProductNumber('case', 59, true);
